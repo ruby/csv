a822828d4445c7acd77f59a9b948c8cd3098d813
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2017-04-24 15:43:34 +0900

    overrided boilerplate by bundle init cmath

---
+++ lib/csv/version.rb
@@ -0,0 +1,3 @@
+module Csv
+  VERSION = "0.1.0"
+end
---
+++ test/csv_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CsvTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Csv::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
