248241516c6958b0412ffe93ea3fa419761e8102
Kouhei Sutou <kou@clear-code.com>
2018-12-20 11:51:42 +0900

    travis: add a test for gem

---
+++ run-test.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift("test")
+$LOAD_PATH.unshift("test/lib")
+$LOAD_PATH.unshift("lib")
+
+Dir.glob("test/csv/**/*test_*.rb") do |test_rb|
+  require File.expand_path(test_rb)
+end
