0eee23e4d1221915b57da1624398f8dc1c9d82bb
Kouhei Sutou <kou@clear-code.com>
2018-12-21 23:53:40 +0900

    test: split liberal_parsing tests

--- test/csv/test_features.rb
+++ test/csv/test_features.rb
@@ -157,44 +157,68 @@ line,4,jkl
     assert_equal(3, count)
   end
 
-  def test_liberal_parsing
+  def test_liberal_parsing_middle_quote_start
     input = '"Johnson, Dwayne",Dwayne "The Rock" Johnson'
-    assert_raise(CSV::MalformedCSVError) do
+    error = assert_raise(CSV::MalformedCSVError) do
         CSV.parse_line(input)
     end
+    assert_equal("Illegal quoting in line 1.",
+                 error.message)
     assert_equal(["Johnson, Dwayne", 'Dwayne "The Rock" Johnson'],
                  CSV.parse_line(input, liberal_parsing: true))
+  end
 
+  def test_liberal_parsing_middle_quote_end
     input = '"quoted" field'
-    assert_raise(CSV::MalformedCSVError) do
+    error = assert_raise(CSV::MalformedCSVError) do
         CSV.parse_line(input)
     end
+    assert_equal("Do not allow except col_sep_split_separator " +
+                 "after quoted fields in line 1.",
+                 error.message)
     assert_equal(['"quoted" field'],
                  CSV.parse_line(input, liberal_parsing: true))
+  end
 
-    assert_raise(CSV::MalformedCSVError) do
+  def test_liberal_parsing_quote_after_column_separator
+    error = assert_raise(CSV::MalformedCSVError) do
       CSV.parse_line('is,this "three," or four,fields', liberal_parsing: true)
     end
+    assert_equal("Unclosed quoted field in line 1.",
+                 error.message)
+  end
 
+  def test_liberal_parsing_quote_before_column_separator
     assert_equal(["is", 'this "three', ' or four"', "fields"],
-      CSV.parse_line('is,this "three, or four",fields', liberal_parsing: true))
+                 CSV.parse_line('is,this "three, or four",fields',
+                                liberal_parsing: true))
+  end
 
-    assert_equal(["1", "\"Hamlet says, \\\"Seems", "\\\" madam! Nay it is; I know not \\\"seems.\\\"\""],
-      CSV.parse_line('1,"Hamlet says, \"Seems,\" madam! Nay it is; I know not \"seems.\""', liberal_parsing: true))
+  def test_liberal_parsing_backslash_quote
+    assert_equal([
+                   "1",
+                   "\"Hamlet says, \\\"Seems",
+                   "\\\" madam! Nay it is; I know not \\\"seems.\\\"\"",
+                 ],
+                 CSV.parse_line('1,' +
+                                '"Hamlet says, \"Seems,' +
+                                '\" madam! Nay it is; I know not \"seems.\""',
+                                liberal_parsing: true))
+  end
 
-    assert_equal(["one", "two\"", "three", "four"],
-      CSV.parse_line('one,two",three,four', liberal_parsing: true))
-
-    input = <<~'CSV'
+  def test_liberal_parsing_space_quote
+    input = <<~CSV
       Los Angeles,   34°03'N,    118°15'W
       New York City, 40°42'46"N, 74°00'21"W
       Paris,         48°51'24"N, 2°21'03"E
     CSV
     assert_equal(
-      [["Los Angeles", "   34°03'N", "    118°15'W"],
-       ["New York City", " 40°42'46\"N", " 74°00'21\"W"],
-       ["Paris", "         48°51'24\"N", " 2°21'03\"E"]],
-      CSV.parse(input, liberal_parsing: true))
+                 [
+                   ["Los Angeles", "   34°03'N", "    118°15'W"],
+                   ["New York City", " 40°42'46\"N", " 74°00'21\"W"],
+                   ["Paris", "         48°51'24\"N", " 2°21'03\"E"],
+                 ],
+                 CSV.parse(input, liberal_parsing: true))
   end
 
   def test_csv_behavior_readers
