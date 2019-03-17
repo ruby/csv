492e88e3f297d008cbdb05eb2b4d9a4fb0bfb5f1
Kouhei Sutou <kou@clear-code.com>
2019-03-17 18:03:45 +0900

    Split tests

--- test/csv/interface/test_delegation.rb
+++ test/csv/interface/test_delegation.rb
@@ -3,16 +3,45 @@
 require_relative "../helper"
 
 class TestCSVInterfaceDelegation < Test::Unit::TestCase
-  def test_stringio_missing_methods_delegation
-    csv = CSV.new("h1,h2")
+  class TestStringIO < self
+    def setup
+      @csv = CSV.new("h1,h2")
+    end
 
-    assert_raise(NotImplementedError) { csv.flock(0) }
-    assert_raise(NotImplementedError) { csv.ioctl(0) }
-    assert_raise(NotImplementedError) { csv.stat }
-    assert_raise(NotImplementedError) { csv.to_i }
+    def test_flock
+      assert_raise(NotImplementedError) do
+        @csv.flock(File::LOCK_EX)
+      end
+    end
 
-    assert_equal(false, csv.binmode?)
-    assert_equal(nil, csv.path)
-    assert_instance_of(StringIO, csv.to_io)
+    def test_ioctl
+      assert_raise(NotImplementedError) do
+        @csv.ioctl(0)
+      end
+    end
+
+    def test_stat
+      assert_raise(NotImplementedError) do
+        @csv.stat
+      end
+    end
+
+    def test_to_i
+      assert_raise(NotImplementedError) do
+        @csv.to_i
+      end
+    end
+
+    def test_binmode?
+      assert_equal(false, @csv.binmode?)
+    end
+
+    def test_path
+      assert_equal(nil, @csv.path)
+    end
+
+    def test_to_io
+      assert_instance_of(StringIO, @csv.to_io)
+    end
   end
 end
