007b9739b006cb5a61901a41955420b1254d0dc4
Kouhei Sutou <kou@clear-code.com>
2019-05-23 06:07:32 +0900

    test: don't change $, with Ruby 2.7
    Because using $, with non-nil is deprecated.

--- test/lib/with_different_ofs.rb
+++ test/lib/with_different_ofs.rb
@@ -1,18 +1,31 @@
 # frozen_string_literal: false
+
 module DifferentOFS
-  module WithDifferentOFS
-    def setup
-      super
-      @ofs, $, = $,, "-"
-    end
-    def teardown
-      $, = @ofs
-      super
-    end
+  is_output_field_separator_deprecated = false
+  stderr, $stderr = $stderr, StringIO.new
+  begin
+    ofs, $, = $,, "-"
+    is_output_field_separator_deprecated = (not $stderr.string.empty?)
+  ensure
+    $, = ofs
+    $stderr = stderr
   end
 
-  def self.extended(klass)
-    super(klass)
-    klass.const_set(:DifferentOFS, Class.new(klass).class_eval {include WithDifferentOFS}).name
+  unless is_output_field_separator_deprecated
+    module WithDifferentOFS
+      def setup
+        super
+        @ofs, $, = $,, "-"
+      end
+      def teardown
+        $, = @ofs
+        super
+      end
+    end
+
+    def self.extended(klass)
+      super(klass)
+      klass.const_set(:DifferentOFS, Class.new(klass).class_eval {include WithDifferentOFS}).name
+    end
   end
 end
