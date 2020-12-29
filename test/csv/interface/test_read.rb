ef5594865ccf2481fead4bcb4fe2f184447af84e
Nobuyoshi Nakada <nobu@ruby-lang.org>
2020-12-29 11:57:53 +0900

    Allow :newline option to CSV.open
    As `:newline` has priority over `:universal_newline` etc. in
    `IO.open`, so deal with both.  On the other hand, such options
    cannot be passed to `CSV#initialize`.

--- test/csv/interface/test_read.rb
+++ test/csv/interface/test_read.rb
@@ -187,6 +187,18 @@ class TestCSVInterfaceRead < Test::Unit::TestCase
     end
   end
 
+  def test_open_with_newline
+    CSV.open(@input.path, col_sep: "\t", universal_newline: true) do |csv|
+      assert_equal(@rows, csv.to_a)
+    end
+    File.open(@input.path, "w") do |file|
+      file << "1,2,3\r\n" "4,5\n"
+    end
+    CSV.open(@input.path, newline: :universal) do |csv|
+      assert_equal(@rows, csv.to_a)
+    end
+  end
+
   def test_parse
     assert_equal(@rows,
                  CSV.parse(@data, col_sep: "\t", row_sep: "\r\n"))
