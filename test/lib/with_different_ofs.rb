d2b0f90eccc86748ffaa8045aeeb4f873a1395e1
Sutou Kouhei <kou@clear-code.com>
2021-06-04 16:35:27 +0900

    Don't run tests for $, when $, is deprecated

--- test/lib/with_different_ofs.rb
+++ test/lib/with_different_ofs.rb
@@ -1,22 +1,34 @@
 # frozen_string_literal: true
+
 module DifferentOFS
-  module WithDifferentOFS
-    def setup
-      super
-      verbose, $VERBOSE = $VERBOSE, nil
-      @ofs, $, = $,, "-"
-      $VERBOSE = verbose
-    end
-    def teardown
-      verbose, $VERBOSE = $VERBOSE, nil
-      $, = @ofs
-      $VERBOSE = verbose
-      super
-    end
+  is_output_field_separator_deprecated = false
+  verbose, $VERBOSE = $VERBOSE, true
+  stderr, $stderr = $stderr, StringIO.new
+  begin
+    ofs, $, = $,, "-"
+    is_output_field_separator_deprecated = (not $stderr.string.empty?)
+  ensure
+    $, = ofs
+    $stderr = stderr
+    $VERBOSE = verbose
   end
 
-  def self.extended(klass)
-    super(klass)
-    klass.const_set(:DifferentOFS, Class.new(klass).class_eval {include WithDifferentOFS})
+  unless is_output_field_separator_deprecated
+    module WithDifferentOFS
+      def setup
+        super
+        @ofs, $, = $,, "-"
+      end
+
+      def teardown
+        $, = @ofs
+        super
+      end
+    end
+
+    def self.extended(klass)
+      super(klass)
+      klass.const_set(:DifferentOFS, Class.new(klass).class_eval {include WithDifferentOFS})
+    end
   end
 end
