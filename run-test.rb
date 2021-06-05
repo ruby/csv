a01c8f2e5a0c74bc40c04a03df2bac7064193430
Kevin Newton <kevin.deisz+github@gmail.com>
2021-06-05 16:23:43 -0400

    Support pattern matching in CSV rows (#207)

--- run-test.rb
+++ run-test.rb
@@ -7,5 +7,8 @@ $LOAD_PATH.unshift("test/lib")
 $LOAD_PATH.unshift("lib")
 
 Dir.glob("test/csv/**/*test_*.rb") do |test_rb|
+  # Ensure we only load syntax that we can handle
+  next if RUBY_VERSION < "2.7" && test_rb.end_with?("test_patterns.rb")
+
   require File.expand_path(test_rb)
 end
