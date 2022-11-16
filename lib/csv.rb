27fb15c498403cba6ec474e5c4139355a737a58d
Mau Magnaguagno <maumagnaguagno@gmail.com>
2022-11-16 02:49:34 -0300

    Remove key sorting in CSV.instance (#262)
    ``DEFAULT_OPTIONS`` is a frozen Hash with symbols as keys.
    There is no need to convert such keys to strings before sorting.
    Note that sorting is not needed for stable implementations of Hash, keys
    would always be returned in the same entry order.
    Sorting removed as all supported Ruby versions implement entry order
    Hashes.
    
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- lib/csv.rb
+++ lib/csv.rb
@@ -1005,7 +1005,7 @@ class CSV
     def instance(data = $stdout, **options)
       # create a _signature_ for this method call, data object and options
       sig = [data.object_id] +
-            options.values_at(*DEFAULT_OPTIONS.keys.sort_by { |sym| sym.to_s })
+            options.values_at(*DEFAULT_OPTIONS.keys)
 
       # fetch or create the instance for this signature
       @@instances ||= Hash.new
