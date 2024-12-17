cdc2b4772bdb50e491ae363ccd02c9a155c55e41
BurdetteLamar <burdettelamar@yahoo.com>
2024-12-17 17:52:46 -0600

    Respond to review

--- test/csv/csv-filter/test_csv_filter.rb
+++ test/csv/csv-filter/test_csv_filter.rb
@@ -2,38 +2,28 @@
 
 require_relative '../helper'
 
-require 'csv'
-
 class TestFilter < Test::Unit::TestCase
 
   def setup
-    @rows = [
-      %w[aaa bbb ccc],
-      %w[ddd eee fff],
-    ]
-  end
-
-  # Return CSV string generated from rows array and options.
-  def make_csv_s(rows: Rows, **options)
-    CSV.generate(**options) do|csv|
-      rows.each do |row|
-        csv << row
-      end
-    end
+    @input = [
+      %w[aaa bbb ccc].join(','),
+      %w[ddd eee fff].join(','),
+      '' # Force trailing newline.
+    ].join("\n")
   end
 
   # Return filepath of file containing CSV data.
-  def csv_filepath(csv_in_s, dirpath, option_sym)
-    filename = "#{option_sym}.csv"
+  def csv_filepath(input, dirpath, option)
+    filename = "#{option}.csv"
     filepath = File.join(dirpath, filename)
-    File.write(filepath, csv_in_s)
+    File.write(filepath, input)
     filepath
   end
 
   # Return stdout and stderr from CLI execution.
   def run_csv_filter(filepath, *cli_option_names)
     top_dir = File.join(__dir__, "..", "..", "..")
-    command_line_s = [
+    command_line = [
       Gem.ruby,
       "-I",
       File.join(top_dir, "lib"), 
@@ -43,7 +33,7 @@ class TestFilter < Test::Unit::TestCase
     ].join(' ')
     Tempfile.create("stdout", mode: File::RDWR) do |stdout|
       Tempfile.create("stderr", mode: File::RDWR) do |stderr|
-        status = system(command_line_s, {1 => stdout, 2 => stderr})
+        status = system(command_line, {1 => stdout, 2 => stderr})
         stdout.rewind
         stderr.rewind
         [stdout.read, stderr.read]
@@ -53,51 +43,50 @@ class TestFilter < Test::Unit::TestCase
 
   # Return results for CLI-only option (or invalid option).
   def results_for_cli_option(option_name)
-    cli_out_s = ''
-    cli_err_s = ''
+    output = ''
+    error = ''
     Dir.mktmpdir do |dirpath|
       sym = option_name.to_sym
       filepath = csv_filepath('', dirpath, sym)
-      cli_out_s, cli_err_s = run_csv_filter(filepath, [option_name])
+      output, error = run_csv_filter(filepath, [option_name])
     end
-    [cli_out_s, cli_err_s]
+    [output, error]
   end
 
   # Get and return the actual output from the API.
-  def get_via_api(csv_in_s, **api_options)
-    cli_out_s = ''
-    CSV.filter(csv_in_s, cli_out_s, **api_options) {|row| }
-    cli_out_s
+  def api_output(input, **api_options)
+    output = ''
+    CSV.filter(input, output, **api_options) {|row| }
+    output
   end
 
   # Test for invalid option.
 
   def test_invalid_option
-    cli_out_s, cli_err_s = results_for_cli_option('-Z')
-    assert_equal("", cli_out_s)
-    assert_match(/OptionParser::InvalidOption/, cli_err_s)
+    output, error = results_for_cli_option('-Z')
+    assert_equal("", output)
+    assert_match(/OptionParser::InvalidOption/, error)
   end
 
   # Test for no options.
 
   def test_no_options
-    csv_in_s = make_csv_s
-    cli_out_s = get_via_api(csv_in_s)
-    assert_equal(csv_in_s, cli_out_s)
+    output = api_output(@input)
+    assert_equal(@input, output)
   end
 
   # Tests for general options.
 
   def test_option_h
     output, error = results_for_cli_option('-h')
-    assert_equal("Usage: csv-filter [options]\n", cli_out_s.lines.first)
-    assert_equal('', cli_err_s)
+    assert_equal("Usage: csv-filter [options]\n", output.lines.first)
+    assert_equal('', error)
   end
 
   def test_option_v
-    cli_out_s, cli_err_s = results_for_cli_option('-v')
-    assert_match(/\d+\.\d+\.\d+/, cli_out_s)
-    assert_equal('', cli_err_s)
+    output, error = results_for_cli_option('-v')
+    assert_match(/\d+\.\d+\.\d+/, output)
+    assert_equal('', error)
   end
 
 end
